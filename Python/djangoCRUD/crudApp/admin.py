7dcc62e26be4fedd2edd4d4357a25e1d277a1821
purba.saha <sahapurba@gmail.com>
2025-05-20 12:29:29 +0530

    initial commit

---
+++ Python/djangoCRUD/crudApp/admin.py
@@ -0,0 +1,10 @@
+
+# Register your models here.
+
+
+from django.contrib import admin
+from .models import Student
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "email"]
